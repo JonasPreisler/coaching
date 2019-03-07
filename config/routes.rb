b17f93a70f0ecfb4eeddd0ebc1b19ccc26d7d622
Jonas <jonas.preisler@g>
2019-03-07 16:08:59 +0800

    Added 3 new pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :accounts
   resources :sub_categories
   resources :categories
+  get '/home', to: 'pages#home'
+  get '/contact1', to: 'pages#contact1'
+  get '/contact2', to: 'pages#contact2'
+  get '/terapeuter', to: 'pages#terapeuter'
   root to: 'categories#index'
   get '/accounts/online', to: 'accounts#online'
 end
