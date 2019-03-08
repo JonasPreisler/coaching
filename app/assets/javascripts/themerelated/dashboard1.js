9cc2da1930a2bf1ac74264208af720ed76e4de8b
Dinesh Budhayer <dbu@logpoint.com>
2019-03-08 22:37:28 +0545

    errors in terapeuter page

--- app/assets/javascripts/themerelated/dashboard1.js
+++ app/assets/javascripts/themerelated/dashboard1.js
@@ -11,58 +11,60 @@ $(function () {
             , stack: 6
         })
         // Dashboard 1 Morris-chart
-    Morris.Area({
-        element: 'morris-area-chart'
-        , data: [{
+    if ($('#morris-area-chart').length) {
+        Morris.Area({
+            element: 'morris-area-chart'
+            , data: [{
                 period: '2010'
                 , iphone: 50
                 , ipad: 80
                 , itouch: 20
-        }, {
+            }, {
                 period: '2011'
                 , iphone: 130
                 , ipad: 100
                 , itouch: 80
-        }, {
+            }, {
                 period: '2012'
                 , iphone: 80
                 , ipad: 60
                 , itouch: 70
-        }, {
+            }, {
                 period: '2013'
                 , iphone: 70
                 , ipad: 200
                 , itouch: 140
-        }, {
+            }, {
                 period: '2014'
                 , iphone: 180
                 , ipad: 150
                 , itouch: 140
-        }, {
+            }, {
                 period: '2015'
                 , iphone: 105
                 , ipad: 100
                 , itouch: 80
-        }
-            , {
-                period: '2016'
-                , iphone: 250
-                , ipad: 150
-                , itouch: 200
-        }]
-        , xkey: 'period'
-        , ykeys: ['iphone', 'ipad', 'itouch']
-        , labels: ['iPhone', 'iPad', 'iPod Touch']
-        , pointSize: 3
-        , fillOpacity: 0
-        , pointStrokeColors: ['#00bfc7', '#fb9678', '#9675ce']
-        , behaveLikeLine: true
-        , gridLineColor: '#e0e0e0'
-        , lineWidth: 3
-        , hideHover: 'auto'
-        , lineColors: ['#00bfc7', '#fb9678', '#9675ce']
-        , resize: true
-    });
+            }
+                , {
+                    period: '2016'
+                    , iphone: 250
+                    , ipad: 150
+                    , itouch: 200
+                }]
+            , xkey: 'period'
+            , ykeys: ['iphone', 'ipad', 'itouch']
+            , labels: ['iPhone', 'iPad', 'iPod Touch']
+            , pointSize: 3
+            , fillOpacity: 0
+            , pointStrokeColors: ['#00bfc7', '#fb9678', '#9675ce']
+            , behaveLikeLine: true
+            , gridLineColor: '#e0e0e0'
+            , lineWidth: 3
+            , hideHover: 'auto'
+            , lineColors: ['#00bfc7', '#fb9678', '#9675ce']
+            , resize: true
+        });
+    }
 });    
     // sparkline
     var sparklineLogin = function() { 
