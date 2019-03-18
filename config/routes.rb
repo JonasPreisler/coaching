4322737d81d7248209ca859370cad9f8cfd5cd5b
Jonas <jonas.preisler@g>
2019-03-19 03:30:19 +0800

    Added online status and modal

--- config/routes.rb
+++ config/routes.rb
@@ -41,7 +41,7 @@ Rails.application.routes.draw do
 
   get "/404", :to => "errors#not_found", :via => :all
   get "/500", :to => "errors#internal_server_error", :via => :all
-get "home/download_pdf"
+  get "home/download_pdf"
 
 
 end
