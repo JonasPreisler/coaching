cd6abb20a3fe0c3ae65c443bb23d4d568f4b3bee
Jonas <jonas.preisler@g>
2019-03-26 01:28:46 +0800

    Changed colmns

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -74,6 +74,6 @@ class BookingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def booking_params
-      params.require(:booking).permit(:account_id, :tutor_id, :datetime, :time, :offer_id)
+      params.require(:booking).permit(:account_id, :tutor_id, :start, :end, :time, :offer_id)
     end
 end
--- db/migrate/20190325141229_create_bookings.rb
+++ db/migrate/20190325141229_create_bookings.rb
@@ -3,7 +3,8 @@ class CreateBookings < ActiveRecord::Migration[5.2]
     create_table :bookings do |t|
       t.integer :account_id
       t.integer :tutor_id
-      t.datetime :datetime
+      t.datetime :start
+      t.datetime :end
       t.integer :offer_id
 
       t.timestamps
