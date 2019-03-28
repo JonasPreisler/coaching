0e46b1e3ac2ae32bb268330dbf7a244a9a3bc2f5
Jonas <jonas.preisler@g>
2019-03-29 00:32:09 +0800

    Timepicker styling

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,19 +3,19 @@ class Booking < ApplicationRecord
 	belongs_to :tutor
 
 
-def time_slots
+	def time_slots
 
-start_time = Time.local(2013, 5, 25, 10, 00) 
-  end_time = Time.local(2013, 5, 25, 20, 00) 
-     times = [start_time.strftime('%H:%M')]
+		start_time = Time.local(2013, 5, 25, 10, 00) 
+		  end_time = Time.local(2013, 5, 25, 20, 00) 
+		     times = [start_time.strftime('%H:%M')]
 
-  begin
-    start_time += 5.minutes
-    times << start_time.strftime('%H:%M')
-  end while start_time < end_time
+	  begin
+	    start_time += 5.minutes
+	    times << start_time.strftime('%H:%M')
+	  end while start_time < end_time
 
-  times
+	  times
 
-end
+	end
 
 end
