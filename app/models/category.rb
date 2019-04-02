efb705b638f2aecc6934b76dd962396ce693b352
Jonas <jonas.preisler@g>
2019-04-03 06:02:30 +0800

    Unique categories and tutors association

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   extend FriendlyId
 	has_many :sub_categories
 	has_many :tutors_categories
-	has_many :tutors, through: :tutors_categories
+	has_many :tutors, -> { distinct }, through: :tutors_categories
   friendly_id :name, use: :slugged
 
 	scope :min_rating, -> (rating) { joins(:reviews).group("tutors.id").having('AVG(reviews.rating) > ?', rating) if rating }
